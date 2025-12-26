4e6110469d2781d91f7d74295a132a3ba142e614
Aadit Yadav <aadityadav121@gmail.com>
2025-12-27 00:12:33 +0530

    Ready to deplpy

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+// console.log("Hello");
+// async function name() {
+//   const response = await fetch(
+//     "https://official-joke-api.appspot.com/jokes/programming/random"
+//   );
+
+//   const data = await response.json();
+//   console.log(data[0].type);
+//   console.log(data[0].setup);
+//   console.log(data[0].punchline);
+// }
+
+// name();
